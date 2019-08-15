9d0a4075678fbab75531f9215d90a18b4a57c9dd
arnold.kyeza <kyezaarnold63@gmail.com>
2019-08-15 13:24:23 +0300

    - Added erp app models Country, City and Person

--- erp/models.py
+++ erp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    country = models.ForeignKey('erp.Country', on_delete=models.CASCADE())
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    country = models.ForeignKey('erp.Country', on_delete=models.SET_NULL, null=True)
+    city = models.ForeignKey('erp.City', on_delete=models.SET_NULL, null=True)
